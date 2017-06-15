5f3b4589743123da74b52cb9797ef5c173116f31
Kaloyan Aleksiev <heuckam@gmail.com>
2017-06-15 23:01:19 +0300

    Create ProductsBundle

---
+++ src/ProductsBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProductsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ProductsBundle:Default:index.html.twig');
+    }
+}
