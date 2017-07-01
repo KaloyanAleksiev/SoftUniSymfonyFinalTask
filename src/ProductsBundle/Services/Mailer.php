a127ad5594f17df63b7d0f664ea16cf3080e2f8f
Kaloyan Aleksiev <heuckam@gmail.com>
2017-07-01 09:52:52 +0300

    Mailer Update

--- src/ProductsBundle/Services/Mailer.php
+++ src/ProductsBundle/Services/Mailer.php
@@ -14,10 +14,15 @@ class Mailer
         $message =  (new \Swift_Message())
             ->setSubject('Send from my first Symfony Application')
             ->setFrom('heuckam@gmail.com', 'Kaloyan Aleksiev')
-            ->setTo('kikoman1@abv.bg','kalexiev@map-mr.com')
             ->setBody('Ko staa maniaci?!?! :) -> (5pts)Create service for sending emails to notify list of predefined email addresses for 
 new products in application.');
-        $numberOfSuccessfullySentMails = $mailer->send($message);
+        $predefinedList = ['kikoman1@abv.bg', 'kalexiev@map-mr.com', 'hprodanov@map-mr.com', 'tgeorgieva@map-mr.com', 'lqlqlq@gbg.bg', 'Maleksieva@c3i-inc.com'];
+        $numberOfSuccessfullySentMails = 0;
+        foreach ($predefinedList as $address => $name)
+        {
+            $message->setTo($name);
+            $numberOfSuccessfullySentMails += $mailer->send($message, $failedRecipients);
+        }
         return $numberOfSuccessfullySentMails;
     }
 }
